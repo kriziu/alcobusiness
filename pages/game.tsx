bfdf300167a24f8df2e77124efbd1d031824aa68
kriziu <brunodzi07@gmail.com>
2022-09-19 22:08:17 +0200

    Add window resize handler

--- pages/game.tsx
+++ pages/game.tsx
@@ -3,18 +3,33 @@ import { useEffect } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
+import { useMobileMode } from '@/common/recoil/mobileMode';
 import { usePlayers } from '@/common/recoil/players';
 import Board from '@/modules/board';
 import PlayerList from '@/modules/interface';
 
 const GamePage: NextPage = () => {
   const { players } = usePlayers();
+  const { setMobileMode } = useMobileMode();
 
   const router = useRouter();
 
   useEffect(() => {
     if (!players.length) router.push('/');
   }, [players, router]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1400) setMobileMode(true);
+      else setMobileMode(false);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [setMobileMode]);
 
   return (
     <div className="flex h-full w-full">
