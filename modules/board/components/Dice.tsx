13fde028783f2d87302309aee208cea1029a3ac8
kriziu <brunodzi07@gmail.com>
2022-10-04 17:35:18 +0200

    Add card shine when double

--- modules/board/components/Dice.tsx
+++ modules/board/components/Dice.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
+import { motion } from 'framer-motion';
 import {
   BsDice1Fill,
   BsDice2Fill,
@@ -11,6 +12,8 @@ import {
 import { IoDice } from 'react-icons/io5';
 
 import { useMobileMode } from '@/common/recoil/mobileMode';
+
+let done = false;
 
 const Dice = ({
   dice,
@@ -30,6 +33,7 @@ const Dice = ({
     if (dice === 0) {
       setTempDice(dice);
       setTempDice2(dice);
+      done = false;
     }
   }, [dice]);
 
@@ -47,12 +51,15 @@ const Dice = ({
   const handleRollDice = () => {
     if (!tempDice)
       for (let i = 0; i < 25; i += 1)
+        // eslint-disable-next-line @typescript-eslint/no-loop-func
         setTimeout(() => {
           const newDice = Math.floor(Math.random() * 6) + 1;
           setTempDice(newDice);
 
           const newDice2 = Math.floor(Math.random() * 6) + 1;
           setTempDice2(newDice2);
+
+          if (i === 24) done = true;
         }, i * 25);
   };
 
@@ -76,7 +83,7 @@ const Dice = ({
   };
 
   return (
-    <button
+    <motion.button
       className={`button flex h-24 items-center justify-center
       ${
         !mobileMode.turned &&
@@ -85,10 +92,27 @@ const Dice = ({
       ${!tempDice ? 'gap-2' : 'gap-10'}`}
       onClick={handleRollDice}
       style={{ fontSize: !tempDice ? '4rem' : '3rem' }}
+      animate={{
+        backgroundImage:
+          tempDice === tempDice2 && done
+            ? [
+                'linear-gradient(45deg, #3fff00, #269900)',
+                'linear-gradient(45deg, #269900, #3fff00)',
+              ]
+            : [
+                'linear-gradient(45deg, #f59e0b, #f43f5e)',
+                'linear-gradient(45deg, #f59e0b, #f43f5e)',
+              ],
+      }}
+      transition={{
+        duration: 0.5,
+        repeat: Infinity,
+        repeatType: 'reverse',
+      }}
     >
       {renderDice(tempDice)}
       {renderDice(tempDice2)}
-    </button>
+    </motion.button>
   );
 };
 
